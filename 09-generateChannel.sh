5f822d2ebd188cc5c2690599ada72606785e2ca3
Maritere Nieto <mtnieto94@gmail.com>
2020-01-21 16:31:56 +0100

    works

--- 09-generateChannel.sh
+++ 09-generateChannel.sh
@@ -8,4 +8,7 @@ mv org0/orderer/tls-msp/keystore/* org0/orderer/tls-msp/keystore/key.pem
 mv org1/peer1/tls-msp/keystore/* org1/peer1/tls-msp/keystore/key.pem
 mv org1/peer2/tls-msp/keystore/* org1/peer2/tls-msp/keystore/key.pem
 mv org2/peer1/tls-msp/keystore/* org2/peer1/tls-msp/keystore/key.pem
-mv org2/peer1/tls-msp/keystore/* org2/peer2/tls-msp/keystore/key.pem
+mv org2/peer2/tls-msp/keystore/* org2/peer2/tls-msp/keystore/key.pem
+mkdir -p org1/admin/msp/admincerts
+cp org1/admin/msp/signcerts/cert.pem org1/admin/msp/admincerts
+cp org0/orderer/tls-msp/tlscacerts/tls-0-0-0-0-8052.pem org1/peer1/msp
