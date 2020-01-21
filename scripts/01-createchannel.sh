47965630a52440752edd22b41ff1058e26ecbaee
Maritere Nieto <mtnieto94@gmail.com>
2020-01-21 13:17:41 +0100

    Updated gitignore

--- scripts/01-createchannel.sh
+++ scripts/01-createchannel.sh
@@ -1,5 +1,5 @@
 CORE_PEER_LOCALMSPID="org1MSP"
-CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/peer1/msp
+CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/admin/msp
 
 peer channel create -c channel1 -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/channel1.tx -o orderer-org0:7050 --outputBlock /tmp/hyperledger/org1/peer1/assets/mychannel.block --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
 
